86d5e744fc4f1b99555a5a17517f23c8e574fe2d
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2024-04-21 17:39:16 +0200

    feat: 1st exercise

--- src/exercises/sum_all_numbers_in_range.rs
+++ src/exercises/sum_all_numbers_in_range.rs
@@ -7,7 +7,8 @@
 #[allow(dead_code)]
 #[allow(unused_variables)]
 pub fn sum_all(arr: &mut [i32; 2]) -> i32 {
-    unimplemented!()
+    arr.sort();
+    (arr[0]..=arr[1]).sum();
 }
 
 #[cfg(test)]
@@ -17,12 +18,11 @@ mod tests {
     #[test]
     fn test1() {
         let mut arg = [1, 4];
-
         assert_eq!(sum_all(&mut arg), 10);
     }
 
     #[test]
-    fn test2() {
+    fn test() {
         let mut arg = [4, 1];
 
         assert_eq!(sum_all(&mut arg), 10);
